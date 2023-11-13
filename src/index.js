0b3a0a906d2a7f9c014116e3e90929da9e16fcca
nanaenolab <nanaeitoi@gmail.com>
2023-11-13 16:25:59 -0500

    Added api for city name

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function refreshWeather(response){
+    let tempElement = document.querySelector("#temp");
+    let temp = response.data.temperature.current;
+     let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(temp);
+    
+}
+
+
+function searchCity(city){
+    let apiKey = "050fe34328718327775fb9d4d93odat0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearch(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Paris")

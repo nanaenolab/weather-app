8f94997de46aa63a2295e10514b9161802eda165
nanaenolab <nanaeitoi@gmail.com>
2023-11-13 17:09:18 -0500

    injected more weather info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 function refreshWeather(response){
     let tempElement = document.querySelector("#temp");
     let temp = response.data.temperature.current;
-     let cityElement = document.querySelector("#city");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+    console.log(response.data);
     cityElement.innerHTML = response.data.city;
     tempElement.innerHTML = Math.round(temp);
-    
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+    windElement.innerHTML = `${response.data.wind.speed} km/h`;
+    timeElement.innerHTML = formateDate(date);
 }
-
+function formateDate(date){
+    let minutes = date.getMinutes();
+    let hour = date.getHours();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", ];
+    let day = days[date.getDay()];
+    if(minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hour}:${minutes}`;
+}
 
 function searchCity(city){
     let apiKey = "050fe34328718327775fb9d4d93odat0";
